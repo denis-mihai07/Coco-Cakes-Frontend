5e109dbf9bbb1a29213b8324f4773a39449e3de7
Denis Mihai <birtalanmihaidenis@gmail.com>
2025-12-07 02:37:03 +0200

    schimbare imagini + organizare

--- front_end/src/js/scroll.js
+++ front_end/src/js/scroll.js
@@ -1,17 +1,14 @@
-window.addEventListener('scroll', () => {
-  sessionStorage.setItem('scrollPosition', window.scrollY);
+window.addEventListener("scroll", () => {
+  sessionStorage.setItem("scrollPosition", window.scrollY);
 });
 
-window.addEventListener('load', () => {
-  const scrollPosition = sessionStorage.getItem('scrollPosition');
+window.addEventListener("load", () => {
+  const scrollPosition = sessionStorage.getItem("scrollPosition");
   if (scrollPosition) {
-    setTimeout(() => {
-      window.scrollTo({
-        top: parseInt(scrollPosition),
-        left: 0,
-        behavior: 'instant',
-      });
-    }, 100);
-    
+    window.scrollTo({
+      top: parseInt(scrollPosition),
+      left: 0,
+      behavior: "instant",
+    });
   }
 });
